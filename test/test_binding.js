1a7f29a0cc6704c843edcd89bcc5bd7ce62b561d
Cody Schrank <cody.schrank@gmail.com>
2018-11-20 12:10:07 -0800

    moved index to testing

--- test/test_binding.js
+++ test/test_binding.js
@@ -1,5 +1,6 @@
 const FastTail = require("../dist/binding.js");
 const assert = require("assert");
+const fs = require('fs');
 
 assert(FastTail, "The expected function is undefined");
 
@@ -7,6 +8,21 @@ function testBasic()
 {
     const fastTail = new FastTail("test.txt");
     assert.strictEqual(fastTail.getLogUri(), "test.txt", "Unexpected value returned");
+
+    fastTail.pollRate = 200;
+    assert.strictEqual(fastTail.pollRate, 200, "Unexpected value returned");
+
+    const lines = [];
+
+    fastTail.start((line) => {
+        lines.push(line);
+    }, (index) => {
+        assert.strictEqual(lines.length, index, "Unexpected value returned");
+    })
+
+    setTimeout(() => {
+        fs.appendFileSync('test.txt', '\nyup');
+    }, 200);
 }
 
 assert.doesNotThrow(testBasic, undefined, "testBasic threw an expection");
